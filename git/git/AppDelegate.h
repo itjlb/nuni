d0b73ca6c660ab4a2a88b119889dac7dee162090
itjlb <itjlb@icloud.com>
2016-04-06 01:54:19 +0800

    send

---
+++ git/git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by 蒋利斌 on 16/4/6.
+//  Copyright © 2016年 studyhard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
