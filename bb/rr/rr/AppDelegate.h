d1d6460fa7c7ec86da8da5ed20ad384983c5cbba
itjlb <itjlb@icloud.com>
2016-04-05 10:07:43 +0800

    nu

---
+++ bb/rr/rr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rr
+//
+//  Created by 蒋利斌 on 16/4/5.
+//  Copyright © 2016年 studyhard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bb/rr/rr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rr
+//
+//  Created by 蒋利斌 on 16/4/5.
+//  Copyright © 2016年 studyhard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
